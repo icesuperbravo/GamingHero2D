3fd674660d734c373bbc6a05f99e73695c0d1808
icesuperbravo <icesuperbravo@gmail.com>
2017-04-23 21:23:18 +0300

    first commit

---
+++ Assets/Editor/CameraFollowEditor.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+
+[CustomEditor(typeof(CameraFollow))]
+public class CameraFollowEditor : Editor {
+
+	public override void OnInspectorGUI () 
+	{
+		//base.OnInspectorGUI ();
+		DrawDefaultInspector ();
+
+		CameraFollow cf = (CameraFollow)target;
+
+		if (GUILayout.Button("Set Min Cam Pos")) 
+		{
+			cf.SetMinCamPosition ();
+		}
+
+		if (GUILayout.Button("Set Max Cam Pos")) 
+		{
+			cf.SetMaxCamPosition ();
+		}
+
+	}
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+	
+	public float moveSpeed;
+	public float jumpPower;
+	public bool grounded;
+
+	private Rigidbody2D rb2d;
+	private Animator anim;
+	private float maxMoveSpeed = 3.0f;
+
+	// Use this for initialization
+	void Start () {
+		rb2d = gameObject.GetComponent<Rigidbody2D> ();
+		anim = gameObject.GetComponent<Animator> ();
+	}
+
+	// Update is called once per frame
+	void Update () {
+		anim.SetBool ("Grounded", grounded);
+		anim.SetFloat ("Speed", Mathf.Abs (rb2d.velocity.x));
+		//Space--> jump operation, which makes the panda jump when press space button;
+		if (Input.GetButtonDown ("Jump") && grounded) 
+		{
+			rb2d.AddForce (Vector2.up * jumpPower );
+
+		}
+
+
+	}
+	void FixedUpdate()
+	{
+		//Moving the player
+		float h = Input.GetAxisRaw ("Horizontal");
+		rb2d.AddForce ((Vector2.right * moveSpeed) * h );
+
+		if (rb2d.velocity.x > maxMoveSpeed) 
+		{
+			rb2d.velocity = new Vector2 (maxMoveSpeed, rb2d.velocity.y);
+		}
+
+
+		if (rb2d.velocity.x < -maxMoveSpeed) 
+		{
+			rb2d.velocity = new Vector2 (-maxMoveSpeed, rb2d.velocity.y);
+		}
+
+	}
+}

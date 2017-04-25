ae19ca84734bbe17812e6b7cb7a95bb32a343aea
icesuperbravo <icesuperbravo@gmail.com>
2017-04-25 22:34:39 +0300

    add pauseUI and level switching

---
+++ Assets/Scripts/Door.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Door : MonoBehaviour {
+	public int LevelToLoad;
+
+	private GameMaster gm;
+
+	void Start() 
+	{
+		gm = GameObject.FindGameObjectWithTag ("GameMaster").GetComponent<GameMaster> ();
+	}
+
+	void OnTriggerEnter2D(Collider2D col) {
+		if (col.CompareTag ("Player")) {
+			gm.InputText.text = ("Press 'E' to enter the new world!");
+		}
+		
+	}
+	void OnTriggerStay2D(Collider2D col) {
+		if (col.CompareTag ("Player")) {
+			if (Input.GetKeyDown ("e")) {
+				Application.LoadLevel (LevelToLoad);
+			}
+		}
+
+	}
+	void OnTriggerExit2D(Collider2D col) {
+		if (col.CompareTag ("Player")) {
+			gm.InputText.text = ("");
+		}
+	}
+}
